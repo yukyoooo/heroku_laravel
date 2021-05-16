c36f9c312dcff99f8e55cb3082190be95955003b
yukyoooo <mrym12000@gmail.com>
2021-05-16 19:16:08 +0900

    fix slide.show design

--- resources/views/bookapp/slide/show.blade.php
+++ resources/views/bookapp/slide/show.blade.php
@@ -3,20 +3,38 @@
 @section('content')
 <div class="container" style="margin-top:-40px;">
     <div class="row justify-content-center">
-        <div class="col-12">
+        <div class="col-12 main-wrapper">
             <iframe id="main-slide"
                 class="card-img-top" alt="..."
-                src="{{ $slide->slides_path}}"
+                src="{{ $slide->slides_path}}#view=FitV"
                 scrolling="no"
                 allowtransparency="true"
                 width="100%"
                 height="630"
                 style="border:0;">
             </iframe>
+            <!-- <div class="btn-wrapper">
+                <a id="btn-left" onclick="BtnClickLeft(); return false;" class="btn-left"><i class="fas fa-angle-left"></i></a>
+                <a id="btn-right" onclick="BtnClickRight(); return false;" class="btn-right"><i class="fas fa-angle-right"></i></a>
+                <script>
+                    function BtnClickLeft() {
+                        // alert('左が押されました');
+                        // document.dispatchEvent( new KeyboardEvent( "keyup",{key: '37' })) ;
+                        document.getElementById("btn-left").dispatchEvent( new KeyboardEvent( "keydown", { keyCode: 37 }));
+                    };
+                    function BtnClickRight() {
+                        // alert('右が押されました');
+                        // document.dispatchEvent( new KeyboardEvent( "keyup",{key: '39' })) ;
+                        document.getElementById("main-slide").focus();
+                        document.getElementById("btn-right").dispatchEvent( new KeyboardEvent( "keydown", { keyCode: "a" }));
+
+                    };
+                </script>
+            </div> -->
             <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src="{{ $slide->image_path }}" alt="{{ $slide->book_title }}" width="100%" style="margin:10px; box-shadow: 2px 2px 5px rgba(0,0,0,0.2);">
+                        <img src="{{ $slide->image_path }}" alt="{{ $slide->book_title }}" width="100%" style="margin:20px; box-shadow: 2px 2px 5px rgba(0,0,0,0.2);">
                     </div>
                     <div class="col-md-9 ">
                         <div class="float-right"style="margin:10px;">
@@ -44,15 +62,17 @@
                         </div>
                         @auth
                             @if( ( $slide->user->id ) === ( Auth::user()->id ) )
+                            <div class="edit_delete_wrapper">
                                 <div class="float-right" style="margin:10px;">
                                     <a class="btn btn-primary" href="{{ route('bookapp.slide.edit', ['id' => $slide->id ]) }}">編集</a>
                                 </div>
                                 <div class="float-right" style="margin:10px;">
                                     <form method="POST" action="{{ route('bookapp.slide.destroy', ['id' => $slide->id ]) }}" id="delete_{{ $slide->id }}">
                                     @csrf
-                                        <a href="#" class="btn btn-danger" data-id="{{ $slide->id }}" onclick="deletePost(this);">削除する</a>
+                                        <a href="#" class="btn btn-danger" data-id="{{ $slide->id }}" onclick="deletePost(this);">削除</a>
                                     </form>
                                 </div>
+                            </div>
                             @endif
                         @endauth
                     </div>
