0c91a8bdd08c0096fc5af1461cf079222f87a1aa
yukyoooo <mrym12000@gmail.com>
2021-06-20 14:37:30 +0900

    fix design

--- resources/views/bookapp/user/show.blade.php
+++ resources/views/bookapp/user/show.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container member-slides">
     <div class="row ">
-        <h5>@if($member->nickname) {{$member->nickname}} @else {{ $member->name }} @endifの投稿一覧</h5>
+        <h5 class="member-slides-h5">@if($member->nickname) {{$member->nickname}} @else {{ $member->name }} @endifの投稿一覧</h5>
         @include('bookapp._slides')
     </div>
 </div>
